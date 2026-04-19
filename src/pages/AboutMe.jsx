f4507aefaf0b41e854745b7d55e8d1967ba80286
pavanpavan1138h-cpu <pavanpavan1138h@gmail.com>
2026-04-19 21:25:28 +0530

    feat: restore About Me content and route, removed heading

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -6,9 +6,6 @@ import './AboutMe.css';
 const WhoWeAre = () => {
   return (
     <main className="about-page">
-      <div className="container">
-        <h1 className="section-title about-page-title">About Me</h1>
-      </div>
       <About />
       <Experience />
     </main>
