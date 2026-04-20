ccb7f8348167bd7fa519da467e7e07eed5537f5a
pavanpavan1138h-cpu <pavanpavan1138h@gmail.com>
2026-04-20 21:33:23 +0530

    9

--- src/data/projects.js
+++ src/data/projects.js
@@ -17,64 +17,92 @@ export const projectData = [
     category: 'Commercial',
     image: 'https://lh3.googleusercontent.com/d/1I6SgaZzsXi1MSNLseKYglIc-OIbz7UaY',
     gallery: [
-      'https://drive.google.com/uc?export=download&id=1D-1KaBKUnu9XfVmIis9Jkj55WrIlrNBO',
+      'https://lh3.googleusercontent.com/d/1D-1KaBKUnu9XfVmIis9Jkj55WrIlrNBO',
       'https://lh3.googleusercontent.com/d/1VUTJ1xV3X44wmblsqsCeK9eZqM7jgLC4',
       'https://lh3.googleusercontent.com/d/1sBY4vhty2QA_TZzTDMAYlgBE78IGSoG2',
-      'https://drive.google.com/file/d/1dZAK6BzbRBB6aXc61SoFGwwE2ieJgn3_/view?usp=drive_link',
-      'https://drive.google.com/file/d/1b-6z_8N0oiKbFAdxSRzMdsG9Pc6Esowk/view?usp=sharing',
-      'https://drive.google.com/file/d/1aid9Ey-bT8Y7I8uDy9uyDa0ZU6ei9vXI/view?usp=sharing'
+      'https://lh3.googleusercontent.com/d/1dZAK6BzbRBB6aXc61SoFGwwE2ieJgn3_',
+      'https://lh3.googleusercontent.com/d/1b-6z_8N0oiKbFAdxSRzMdsG9Pc6Esowk',
+      'https://lh3.googleusercontent.com/d/1aid9Ey-bT8Y7I8uDy9uyDa0ZU6ei9vXI'
     ]
   },
   {
     id: 3,
     title: 'Timber Pavilion',
     category: 'Public Space',
-    image: 'https://drive.google.com/file/d/1Yjnbi8HloYWXV_dOube4EZ0O0VehgEt3/view?usp=sharing',
+    image: 'https://lh3.googleusercontent.com/d/1Yjnbi8HloYWXV_dOube4EZ0O0VehgEt3',
     gallery: [
-      'https://drive.google.com/file/d/1OPK4UWzZ-eYp_DAaBRceiDFqCRUeiDvI/view?usp=sharing',
-      'https://drive.google.com/file/d/1hqhA78qoM4YUl9Thpj3MinVpioYG740Y/view?usp=sharing',
-      'https://drive.google.com/file/d/1PaS6EGiyGPDVroJLE3U-LDxxCy5Pexh-/view?usp=sharing',
-      'https://drive.google.com/file/d/1H43UwaKS6EM-icVbpew9Dgvh02VrRyvk/view?usp=sharing',
-      'https://drive.google.com/file/d/1Q46Y94WpDrNKlp_0BjtFQTquW36grmMY/view?usp=sharing',
-      'https://drive.google.com/file/d/10sTzUN3_2_Hh3nWoJsWLxuVpoEw5mjke/view?usp=sharing'
+      'https://lh3.googleusercontent.com/d/1OPK4UWzZ-eYp_DAaBRceiDFqCRUeiDvI',
+      'https://lh3.googleusercontent.com/d/1hqhA78qoM4YUl9Thpj3MinVpioYG740Y',
+      'https://lh3.googleusercontent.com/d/1PaS6EGiyGPDVroJLE3U-LDxxCy5Pexh-',
+      'https://lh3.googleusercontent.com/d/1H43UwaKS6EM-icVbpew9Dgvh02VrRyvk',
+      'https://lh3.googleusercontent.com/d/1Q46Y94WpDrNKlp_0BjtFQTquW36grmMY',
+      'https://lh3.googleusercontent.com/d/10sTzUN3_2_Hh3nWoJsWLxuVpoEw5mjke'
     ]
   },
   {
     id: 4,
     title: 'Glass Retreat',
     category: 'Hospitality',
-    image: 'https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?q=80&w=2075&auto=format&fit=crop',
-    gallery: []
+    image: 'https://lh3.googleusercontent.com/d/1YfJsC4fh4kktXjqC9XiF1rHpQCYGRs63',
+    gallery: [
+      'https://lh3.googleusercontent.com/d/1U-g3J7CKTI6RSw8l9cHlYMcCzpndYf6U',
+      'https://lh3.googleusercontent.com/d/1taLFZeBbg_pK9IFuBY-3LifuhiXC1Ypw',
+      'https://lh3.googleusercontent.com/d/1oilZhIVpkRw-7QgXLrkY6IE4lXpvPxxR',
+      'https://lh3.googleusercontent.com/d/1KzTasMAbS83X005mW-CGI4Y0acgP7B_F',
+      'https://lh3.googleusercontent.com/d/13XR8fKOyyGgQf2TomVIEp0mhUGlhwmMb'
+    ]
   },
   {
     id: 5,
     title: 'Modern Concrete Villa',
     category: 'Residential',
-    image: 'https://images.unsplash.com/photo-1481253127861-534498168948?q=80&w=2073&auto=format&fit=crop',
+    image: 'https://lh3.googleusercontent.com/d/1CDljv3QGVyC4yQmmFa9JQDu9GZfjUozg',
     gallery: [
-      
+      'https://lh3.googleusercontent.com/d/1SzoY8TWLbn7K_Y2-Rm8gBS6etmTgFLFr',
+      'https://lh3.googleusercontent.com/d/1R4zR2vfoy63gHgO_aOWDVHaFCer_wn4m',
+      'https://lh3.googleusercontent.com/d/1vvaQzKwXNN_xx8H_sgxNEvP8KfLetKRl',
+      'https://lh3.googleusercontent.com/d/1vTbYlvsXGRTiT9Fxs5EjEae2Ea2f1nd_',
+      'https://lh3.googleusercontent.com/d/1ZgXBzll7lJjSQoRm6rcF2LYf7mFUVrA_',
+      'https://lh3.googleusercontent.com/d/18YzJCAYSrsap6WbCTWCZ8OGUFb1E4FU6',
+      'https://lh3.googleusercontent.com/d/1BE9ydE1A8ssTFcaxCJqsIFVcDPDrA6i2'
     ]
   },
   {
     id: 6,
     title: 'Skyline Cultural Center',
     category: 'Public Space',
-    image: 'https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?q=80&w=2070&auto=format&fit=crop',
-    gallery: []
+    image: 'https://lh3.googleusercontent.com/d/18VQ5XWHJeH1TtbShMJ_dCETcdPfNWEqu',
+    gallery: [
+      'https://lh3.googleusercontent.com/d/1BZm__C_YOUCerv_AlJKmv_g40GCc-h7I',
+      'https://lh3.googleusercontent.com/d/1QAjefDAREZj01VyDPXiSCFls_jbS7eW2',
+      'https://lh3.googleusercontent.com/d/1luCC7uT46qb04S0D3XFfySLI9BlcanwY',
+      'https://lh3.googleusercontent.com/d/1TJiET2_UkyOjMv51RsDyBlmzQpU2d4m9',
+      'https://lh3.googleusercontent.com/d/127q0RGGZA2pQr47kDHtuiAXTntXLT7sG',
+      'https://lh3.googleusercontent.com/d/1FSn46V5tWZ1s2J9TJVdNvaO7jGONFoQo',
+      'https://lh3.googleusercontent.com/d/14jMq5D7dBN9Mfn0HGvbe_inuGxl6nuJo'
+    ]
   },
   {
     id: 7,
     title: 'Nordic Cabin',
     category: 'Residential',
-    image: 'https://images.unsplash.com/photo-1518780664697-55e3ad937233?q=80&w=2065&auto=format&fit=crop',
-    gallery: []
+    image: 'https://lh3.googleusercontent.com/d/1jDuw5vdXS8nO9MFTZyJcmB_4pyWyNaga',
+    gallery: [
+      'https://lh3.googleusercontent.com/d/1D4dEhlcbq2-3bOf4KzyCoNrLhEZYVwfb',
+      'https://lh3.googleusercontent.com/d/1C-z50Eb_0GGZqNKFLwMFgbuFRPpPRvWr',
+      'https://lh3.googleusercontent.com/d/1nV72_5iBJ34y5u3SmZxMUVlDsKQLhzoO'
+    ]
   },
   {
     id: 8,
     title: 'Artisan Workshop',
     category: 'Commercial',
-    image: 'https://images.unsplash.com/photo-1497366216548-37526070297c?q=80&w=2069&auto=format&fit=crop',
-    gallery: []
+    image: 'https://lh3.googleusercontent.com/d/1qd9Q0cwFZwLyNLHH4FNO_klUdyOpiRzi',
+    gallery: [
+      'https://lh3.googleusercontent.com/d/16KTbdq19ejSWfGv69yTN9px7232DU1xn',
+      'https://lh3.googleusercontent.com/d/13e4sjisUQOnifT1I9-KqKcgJggq7EjL_',
+      'https://lh3.googleusercontent.com/d/1XFCBIBd3NjRVAhxw7tvvyqhYPUm6zjRz'
+    ]
   },
   {
     id: 9,
